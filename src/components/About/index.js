df2d68f3c869c36e10adf84158e41f02f4d4f41e
Elijah Glascock <ejglascock@gmail.com>
2022-08-23 15:26:56 -0700

    and of course the very next thing to do is add it all right back in

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,24 @@
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
     const aboutArray = ['A', 'b', 'o', 'u', 't', ' ', 'm', 'e'];
+
+    useEffect(() => {
+        
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
 
     return (
         <div className="container about-page">
             <div className="text-zone">
                 <h1>
-                    <AnimatedLetters strArray={aboutArray} idx={15} /> 
+                    <AnimatedLetters letterClass={letterClass} strArray={aboutArray} idx={15} /> 
                 </h1>
                 <p>
                     I'm pretty cool
