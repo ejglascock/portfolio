9809991aa940ece104f901fafb37c39c42c69715
Elijah Glascock <ejglascock@gmail.com>
2022-08-24 08:43:01 -0700

    added scss for contact page elements

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -24,6 +24,34 @@ const Contact = () => {
                             idx={15}
                         />
                     </h1>
+                    <p>
+                        I am interested in a full time job as a junior developer. If you have
+                        any requests or questions, don't hesitate to contact me using the form 
+                        below either.
+                    </p>
+                    <div className="contact-form" >
+                        <form>
+                            <ul>
+                                <li className="half" >
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className="half" >
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input placeholder="Subject" type="text" name="subject" required />
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required >
+
+                                    </textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type="pacman" />
