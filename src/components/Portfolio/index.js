62d09f1e4d6fc60b2434dde7c57376f71f13f406
Elijah Glascock <ejglascock@gmail.com>
2022-08-25 08:43:15 -0700

    added css for portfolio page and page title

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
 
 const Portfolio = () => {
-    return <div>Hello</div>;
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={['P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']} 
+                        idx={15}
+                    />
+                </h1>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
 }
 
 export default Portfolio
