719a292f20648d844ac2a76d2528989e290b1c16
Elijah Glascock <ejglascock@gmail.com>
2022-08-25 12:20:56 -0700

    created render portfolio method

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
+import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    console.log(portfolioData);
 
     useEffect(() => {
         
@@ -12,6 +14,27 @@ const Portfolio = () => {
             setLetterClass('text-animate-hover')
         }, 4000)
     }, [])
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx} >
+                                <img 
+                                    src={port.cover}
+                                    className="portfolio-image"
+                                    alt="portfolio" 
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
 
     return (
         <>
@@ -23,7 +46,7 @@ const Portfolio = () => {
                         idx={15}
                     />
                 </h1>
-                <div>{renderPortfolio()}</div>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
             </div>
             <Loader type="pacman" />
         </>
