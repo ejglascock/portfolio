3091bfe060e23e84aeea6d47641a2895ad715f6f
Elijah Glascock <ejglascock@gmail.com>
2022-08-25 12:36:17 -0700

    added content div below portfolio image that includes a title paragraph description h4 and a button that all come from the portfolio data

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -27,6 +27,14 @@ const Portfolio = () => {
                                     className="portfolio-image"
                                     alt="portfolio" 
                                 />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button 
+                                        className="button" 
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
                             </div>
                         )
                     })
