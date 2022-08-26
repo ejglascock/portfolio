493de7c2bccb14a8c98a5d1214e85d474252e175
Elijah Glascock <ejglascock@gmail.com>
2022-08-26 14:41:24 -0700

    removed the h1 pseudo tags as I didn't find them appealing on the home page. Might remove them from all pages. Also included an opening html pseudo tag as I felt it was missing

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,14 +7,18 @@ const Layout = () => {
         <div className="App">
             <Sidebar />
             <div className="page">
-                <span className="tags top-tags">&lt;body&gt;</span>
+                <span className="tags top-tags">
+                    <span className="top-tag-html">&lt;html&gt;</span>
+                    <br />
+                    &lt;body&gt;
+                </span>
 
                 <Outlet />
 
                 <span className="tags bottom-tags">
-                &lt;/body&gt;
-                <br />
-                <span className="bottom-tag-html">&lt;/html&gt;</span>
+                    &lt;/body&gt;
+                    <br />
+                    <span className="bottom-tag-html">&lt;/html&gt;</span>
                 </span>
             </div>
         </div>
