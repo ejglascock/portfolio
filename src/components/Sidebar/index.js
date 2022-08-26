874b98a1e1e8fe420036bd649602c084da993540
Elijah Glascock <ejglascock@gmail.com>
2022-08-26 10:26:47 -0700

    close icon now closes the nav bar, and the nav bar closes when a link is clicked. The links are also colored correctly

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { useState } from 'react';
 
@@ -18,17 +18,24 @@ const Sidebar = () => {
             </Link>
             <nav className={showNav ? 'mobile-show' : ''}>
                 <NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faHome} color="#4d4d4e" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faUser} color="#4d4d4e" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                    <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
                 <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
+                <FontAwesomeIcon
+                    onClick={() => setShowNav(false)} 
+                    icon={faClose}
+                    color='#CAD2C5'
+                    size='3x'
+                    className='close-icon'
+                />
             </nav>
             <ul>
                 <li>
