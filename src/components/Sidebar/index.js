753bb41a10a7bde616d343ce6b3ff7672070e04c
Elijah Glascock <ejglascock@gmail.com>
2022-08-26 10:33:04 -0700

    fixed issue where nav bar would only close when clicking on the icon and not the words for a link

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,17 +17,17 @@ const Sidebar = () => {
                 <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
             </Link>
             <nav className={showNav ? 'mobile-show' : ''}>
-                <NavLink exact="true" activeclassname="active" to="/">
-                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faHome} color="#4d4d4e" />
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faUser} color="#4d4d4e" />
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faSuitcase} color="#4d4d4e" />
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                    <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faEnvelope} color="#4d4d4e" />
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
                 <FontAwesomeIcon
                     onClick={() => setShowNav(false)} 
